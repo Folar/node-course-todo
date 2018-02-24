c5cb5081e8aa9135d959b1e3a796d58411ee461c
Lawrence Ackner <lmackner@gmail.com>
2018-02-24 17:43:55 -0500

    add async await

--- server/models/user.js
+++ server/models/user.js
@@ -45,20 +45,21 @@ UserSchema.methods.generateAuthToken = function () {
   var access = 'auth';
   var token = jwt.sign({_id: user._id.toHexString(), access}, process.env.JWT_SECRET).toString();
 
-  user.tokens = user.tokens.concat({access, token});
+  user.tokens =user.tokens.concat({access, token});
 
   return user.save().then(() => {
     return token;
   });
 };
-UserSchema.methods.removeToken = function (token) {
-    var user = this;
 
-    return user.update({
-        $pull: {
-            tokens: {token}
-        }
-    });
+UserSchema.methods.removeToken = function (token) {
+  var user = this;
+
+  return user.update({
+    $pull: {
+      tokens: {token}
+    }
+  });
 };
 
 UserSchema.statics.findByToken = function (token) {
@@ -77,26 +78,28 @@ UserSchema.statics.findByToken = function (token) {
     'tokens.access': 'auth'
   });
 };
+
 UserSchema.statics.findByCredentials = function (email, password) {
-    var User = this;
+  var User = this;
 
-    return User.findOne({email}).then((user) => {
-        if (!user) {
-            return Promise.reject();
+  return User.findOne({email}).then((user) => {
+    if (!user) {
+      return Promise.reject();
+    }
+
+    return new Promise((resolve, reject) => {
+      // Use bcrypt.compare to compare password and user.password
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          resolve(user);
+        } else {
+          reject();
         }
-
-        return new Promise((resolve, reject) => {
-            // Use bcrypt.compare to compare password and user.password
-            bcrypt.compare(password, user.password, (err, res) => {
-                if (res) {
-                    resolve(user);
-                } else {
-                    reject();
-                }
-            });
-        });
+      });
     });
+  });
 };
+
 UserSchema.pre('save', function (next) {
   var user = this;
 
